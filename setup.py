300450bdc5ce35bdf2d6309d0724a601be1898c4
Byron Ruth <bruth@codeomics.com>
2010-10-15 11:51:33 -0400

    finally fixed setup.py file

--- setup.py
+++ setup.py
@@ -1,47 +1,88 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
+from distutils.command.install_data import install_data
+from distutils.command.install import INSTALL_SCHEMES
 
-kwargs = {
-    'name': 'django-avocado',
-    'version': '1.0',
-    'author': 'Byron Ruth',
-    'author_email': 'ruthb@email.chop.edu',
-    'description': 'A data-driven query engine',
-    'license': 'BSD',
-    'keywords': 'snippets tools utilities',
+import os
+import sys
 
-    'packages': find_packages(exclude=('*.tests', '*.tests.*')),
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
 
-    'package_dir': {
-        'avocado': 'avocado',
-        'client': 'avocado/contrib/client',
-        'server': 'avocado/contrib/server',
-    },
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
 
-    'package_data': {
-        'avocado': ['sql/*.sql'],
+if sys.platform == "darwin": 
+    cmdclasses = {'install_data': osx_install_data} 
+else: 
+    cmdclasses = {'install_data': install_data} 
 
-        'client': [
-            'static/js/src/*',
-            'static/js/src/lib/*',
-            'static/js/src/rest/*',
-            'static/js/src/define/*',
-            'static/js/src/report/*',
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
 
-            'static/js/min/*',
-            'static/js/min/lib/*',
-            'static/js/min/rest/*',
-            'static/js/min/define/*',
-            'static/js/min/report/*',
+# Tell distutils to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
-            'templates/*'
-        ]
-    },
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+avocado_dir = 'avocado'
 
-    'exclude_package_data': {
-        '': ['fixtures/*']
-    },
+for dirpath, dirnames, filenames in os.walk(avocado_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+        elif dirname in ('tests', 'fixtures'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
-    'classifiers': [
+# Small hack for working with bdist_wininst.
+# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
+if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
+    for file_info in data_files:
+        file_info[0] = '\\PURELIB\\%s' % file_info[0]
+
+version = __import__('avocado').__version__
+
+setup(
+    version = version.replace(' ', '-'),
+    name = 'django-avocado',
+    author = 'Byron Ruth',
+    author_email = 'ruthb@email.chop.edu',
+    description = 'A data-driven query engine',
+    license = 'BSD',
+    keywords = 'snippets tools utilities',
+
+    packages = packages,
+    cmdclass = cmdclasses,
+
+    data_files = data_files,
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
@@ -50,6 +91,4 @@ kwargs = {
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
     ],
-}
-
-setup(**kwargs)
+)
