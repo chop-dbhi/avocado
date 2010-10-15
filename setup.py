a456dced39a3841e40c5f8390df1d4f3766f4582
Byron Ruth <bruth@codeomics.com>
2010-10-15 11:15:18 -0400

    updated setup to include data files

--- setup.py
+++ setup.py
@@ -8,13 +8,39 @@ kwargs = {
     'description': 'A data-driven query engine',
     'license': 'BSD',
     'keywords': 'snippets tools utilities',
+
     'packages': find_packages(exclude=('*.tests', '*.tests.*')),
+
+    'package_dir': {
+        'avocado': 'avocado',
+        'client': 'avocado/contrib/client',
+        'server': 'avocado/contrib/server',
+    }
+
     'package_data': {
-	'': ['sql/*', 'static/*', 'templates/*']
+        'avocado': ['sql/*.sql'],
+
+        'client': [
+            'static/js/src/*',
+            'static/js/src/lib/*',
+            'static/js/src/rest/*',
+            'static/js/src/define/*',
+            'static/js/src/report/*',
+
+            'static/js/min/*',
+            'static/js/min/lib/*',
+            'static/js/min/rest/*',
+            'static/js/min/define/*',
+            'static/js/min/report/*',
+
+            'templates/*'
+        ]
     },
+
     'exclude_package_data': {
         '': ['fixtures/*']
     },
+
     'classifiers': [
         'Development Status :: 4 - Beta',
         'Framework :: Django',
