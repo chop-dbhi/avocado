89cd6676e0d803b2e358343feab1bfb0d41ecc2a
Byron Ruth <bruth@codeomics.com>
2010-10-27 09:45:01 -0400

    fixed (hopefully) group issue

--- avocado/columns/managers.py
+++ avocado/columns/managers.py
@@ -18,6 +18,11 @@ class ColumnManager(ConceptManager):
 
     if settings.FIELD_GROUP_PERMISSIONS:
         def restrict_by_group(self, groups):
+            if len(groups) > 0:
+                snp = 'AND U2.group_id not in %s'
+            else:
+                snp = ''
+
             sql = """
             SELECT id
                 FROM avocado_column
@@ -28,7 +33,8 @@ class ColumnManager(ConceptManager):
                                 INNER JOIN avocado_columnfield U1 ON (U0.id = U1.concept_id)
                                 INNER JOIN avocado_field U2 ON (U1.field_id = U2.id)
                             WHERE (U2.is_public = false
-                                OR (U2.group_id is not NULL AND U2.group_id not in %s)))
-            """
+                                OR (U2.group_id is not NULL %s)))
+            """ % snp
+
             return super(ColumnManager, self).restrict_by_group(sql, groups)
 
--- avocado/criteria/managers.py
+++ avocado/criteria/managers.py
@@ -18,6 +18,11 @@ class CriterionManager(ConceptManager):
 
     if settings.FIELD_GROUP_PERMISSIONS:
         def restrict_by_group(self, groups):
+            if len(groups) > 0:
+                snp = 'AND U2.group_id not in %s'
+            else:
+                snp = ''
+
             sql = """
             SELECT id
                 FROM avocado_criterion
@@ -28,7 +33,8 @@ class CriterionManager(ConceptManager):
                                 INNER JOIN avocado_criterionfield U1 ON (U0.id = U1.concept_id)
                                 INNER JOIN avocado_field U2 ON (U1.field_id = U2.id)
                             WHERE (U2.is_public = false
-                                OR (U2.group_id is not NULL AND U2.group_id not in %s)))
-            """
+                                OR (U2.group_id is not NULL %s)))
+            """ % snp
+
             return super(CriterionManager, self).restrict_by_group(sql, groups)
 
