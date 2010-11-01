2b61e97172d4931650f5f8e73bb1f65aa8c02657
Byron Ruth <bruth@codeomics.com>
2010-11-01 09:17:07 -0400

    moved

---
+++ avocado/store/signals.py
@@ -0,0 +1,54 @@
+from avocado.models import Scope, ObjectSet
+
+def objectset_scope_sync_pre_save(sender, instance, **kwargs):
+    """Pre-save handler for both the ``Scope`` object and the ``ObjectSet``
+    object. If registered with ``Scope`` as the sender, two additional keyword
+    arguments must be partially applied prior to connecting the handler.
+
+    ``set_name`` - the reverse name of the ``ObjectSet`` subclass' instance
+
+    ``set_manager`` - the name of the many-to-many manager on the relation
+
+    If the sender is a subclass of ``ObjectSet``, only the ``set_manager``
+    must be defined.
+    """
+    if sender is Scope:
+        try:
+            objectset = getattr(instance, kwargs['set_name'])
+            scope = objectset.scope
+        except (AttributeError, KeyError):
+            return
+    elif issubclass(sender, ObjectSet):
+        objectset = instance
+        if objectset.id is not None:
+            return
+        scope = objectset.scope
+    else:
+        return
+
+    objectset_manager = getattr(objectset, kwargs['set_manager'])
+    oids = set(objectset_manager.distinct().values_list('id', flat=True))
+    nids = set(scope.get_queryset().distinct().values_list('id', flat=True))
+
+    added = (oids | nids) - oids
+    removed = (oids | nids) - nids
+
+    if added:
+        objectset_manager.add(*added)
+    if removed:
+        objectset_manager.remove(*removed)
+
+def objectset_incr_count_pre_save(instance, action, pk_set, **kwargs):
+    """Handlers syncing `cnt` based on the number of objects are being
+    added or removed from the m2m relationship.
+    """
+    if action in ('pre_add', 'pre_remove', 'pre_clear'):
+        if action == 'pre_clear':
+            count = -instance.cnt
+        else:
+            count = len(pk_set)
+            if action == 'pre_remove':
+                count = -count
+
+        instance.cnt += count
+        instance.save()
