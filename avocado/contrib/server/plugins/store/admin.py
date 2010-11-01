b96f803cf0cac9c336775c035e445c9882b7b3a4
Byron Ruth <ruthb@email.chop.edu>
2010-11-01 10:46:07 -0400

    removed legacy plugin

--- avocado/contrib/server/plugins/store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from avocado.contrib.server.plugins.store.models import PatientSet
-
-class PatientSetAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'cnt')
-
-admin.site.register(PatientSet)
