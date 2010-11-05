6d88ecc1c47b16cc8d6e5eaa6c2a1cea6ad7838b
millerjm1@email.chop.edu <millerjm1@email.chop.edu>
2010-11-05 13:24:53 -0400

    Undefined selection bug in IE.

--- avocado/contrib/client/static/js/src/define/form.js
+++ avocado/contrib/client/static/js/src/define/form.js
@@ -86,7 +86,7 @@ require.def('define/form', [], {
                                                         choiceOperatorsTmpl,
                                                      '</select>',
                                                  '<select multiple="multiple" id="<%=this.field_id%>-value" name="<%=this.field_id%>" size="3" data-optional="<%=this.optional%>" >',
-                                                  '<% for (index in this.choices) { %>',
+                                                  '<% for (var index = 0; index < this.choices.length; index++) { %>',
                                                         '<option value="<%=this.choices[index][0]%>"><%=this.choices[index][1]%></option>',
                                                   '<%}%>',
                                                   '</select>',
