175aff8193c94cee849b413a9d2ac2c9314e01a6
Byron Ruth <bruth@codeomics.com>
2010-11-18 10:55:02 -0500

    removed contrib. created cilantro and serrano libraries

--- avocado/contrib/client/static/js/min/login.js
+++
@@ -1,1 +0,0 @@
-define("login",function(){$(function(){function a(){var d=b.val()&&c.val()?false:true;e.attr("disabled",d)}var e=$("[type=submit]"),b=$("[name=username]"),c=$("[name=password]");b.keyup(a);c.keyup(a)})});
