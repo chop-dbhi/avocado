d1c6566c9ca60a67c4936223449a4ab1be96cd07
Byron Ruth <b@devel.io>
2012-02-24 16:32:44 -0500

    Deprecated property

--- avocado/tests/cases/models.py
+++ avocado/tests/cases/models.py
@@ -10,21 +10,18 @@ class FieldTestCase(BaseTestCase):
         self.assertTrue(self.is_manager.field)
         self.assertEqual(self.is_manager.datatype, 'boolean')
         self.assertEqual(self.is_manager.operators, ('exact', '-exact', 'isnull', '-isnull'))
-        self.assertTrue(self.is_manager.has_choices)
 
     def test_integer(self):
         self.assertTrue(self.salary.model)
         self.assertTrue(self.salary.field)
         self.assertEqual(self.salary.datatype, 'number')
         self.assertEqual(self.salary.operators, ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range', 'isnull', '-isnull'))
-        self.assertFalse(self.salary.has_choices)
 
     def test_string(self):
         self.assertTrue(self.first_name.model)
         self.assertTrue(self.first_name.field)
         self.assertEqual(self.first_name.datatype, 'string')
         self.assertEqual(self.first_name.operators, ('exact', '-exact', 'iexact', '-iexact', 'in', '-in', 'icontains', '-icontains'))
-        self.assertFalse(self.first_name.has_choices)
 
 
 class ConceptTestCase(TestCase):
