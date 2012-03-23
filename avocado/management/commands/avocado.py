dfa6d730c7354aadb482552d4057a5e9537dfdd4
Byron Ruth <b@devel.io>
2012-03-23 15:50:00 -0400

    Move subcommands to separate package, allow subcommand options to pass through

--- avocado/management/commands/avocado.py
+++ avocado/management/commands/avocado.py
@@ -1,15 +1,70 @@
-from django.core.management.base import BaseCommand
-from django.core.management import call_command
+import sys
+from optparse import NO_DEFAULT, OptionParser
+from django.core.management.base import BaseCommand, handle_default_options
+from django.utils.importlib import import_module
 
 class Command(BaseCommand):
     help = "A wrapper for Avocado subcommands"
 
     commands = ['sync', 'orphaned']
-    command_prefix = 'avocado_%s'
+
+    def print_subcommands(self, prog_name):
+        usage = ['', 'Available subcommands:']
+        for name in sorted(self.commands):
+            usage.append('  {0}'.format(name))
+        return '\n'.join(usage)
+
+    def usage(self, subcommand):
+        usage = '%prog {0} subcommand [options] [args]'.format(subcommand)
+        if self.help:
+            return '{0}\n\n{1}'.format(usage, self.help)
+        return usage
+
+    def print_help(self, prog_name, subcommand):
+        super(Command, self).print_help(prog_name, subcommand)
+        sys.stdout.write('{0}\n\n'.format(self.print_subcommands(prog_name)))
+
+    def get_subcommand(self, name):
+        try:
+            module = import_module('avocado.management.subcommands.{0}'.format(name))
+            return module.Command()
+        except KeyError:
+            raise CommandError('Unknown subcommand: avocado {0}'.format(name))
+
+    def run_from_argv(self, argv):
+        """Set up any environment changes requested (e.g., Python path
+        and Django settings), then run this command.
+        """
+        if len(argv) > 2 and not argv[2].startswith('-') and argv[2] in self.commands:
+            subcommand = argv[2]
+            klass = self.get_subcommand(subcommand)
+            parser = OptionParser(prog=argv[0], usage=klass.usage('{0} {1}'.format(argv[1], subcommand)),
+                version=klass.get_version(), option_list=klass.option_list)
+            options, args = parser.parse_args(argv[3:])
+            args = [subcommand] + args
+        else:
+            parser = self.create_parser(argv[0], argv[1])
+            options, args = parser.parse_args(argv[2:])
+
+        handle_default_options(options)
+        self.execute(*args, **options.__dict__)
 
     def handle(self, *args, **options):
         if not args or args[0] not in self.commands:
             return self.print_help('./manage.py', 'avocado')
         subcommand, args = args[0], args[1:]
-        return call_command(self.command_prefix % subcommand, *args, **options)
 
+        klass = self.get_subcommand(subcommand)
+        # Grab out a list of defaults from the options. optparse does this for us
+        # when the script runs from the command line, but since call_command can
+        # be called programatically, we need to simulate the loading and handling
+        # of defaults (see #10080 for details).
+        defaults = {}
+        for opt in klass.option_list:
+            if opt.default is NO_DEFAULT:
+                defaults[opt.dest] = None
+            else:
+                defaults[opt.dest] = opt.default
+        defaults.update(options)
+
+        return klass.execute(*args, **defaults)
--- avocado/management/subcommands/sync.py
+++ avocado/management/subcommands/sync.py
@@ -1,10 +1,8 @@
 from optparse import make_option
-
 from django.db.models import (get_model, get_models, get_app, AutoField,
     ForeignKey, OneToOneField, ManyToManyField)
 from django.core.management.base import LabelCommand
-
-from avocado.models import Field, Category
+from avocado.models import Field 
 
 class Command(LabelCommand):
     """
@@ -20,9 +18,6 @@ class Command(LabelCommand):
 
     OPTIONS:
 
-        ``--create-categories`` - Create a single ``Category`` corresponding to each
-        model that is evaluated.
-
         ``--include-non-editable`` - Create ``Field`` instances for fields marked
         as not editable (i.e. ``editable=False``).
 
@@ -35,18 +30,15 @@ class Command(LabelCommand):
 
     """
 
-    help = """Finds all models in the listed app(s) and attempts to create a
-    ``Field`` instance per model field. Fields already declared will
-    not be altered.
-    """
+    help = '\n'.join([
+        'Finds all models in the listed app(s) and attempts to create a',
+        '``Field`` instance per model field. Fields already declared will',
+        'not be altered.'
+    ])
 
-    args = '<app app app.model ...>'
+    args = 'app [app.model, [...]]'
 
     option_list = LabelCommand.option_list + (
-        make_option('--create-categories', action='store_true',
-            dest='create_categories', default=False,
-            help='Create a category for each model'),
-
         make_option('--include-non-editable', action='store_true',
             dest='include_non_editable', default=False,
             help='Create fields for non-editable fields'),
@@ -69,28 +61,17 @@ class Command(LabelCommand):
         OneToOneField,
     )
 
-    def __init__(self, *args, **kwargs):
-        super(Command, self).__init__(*args, **kwargs)
-        self._categories = {}
-
-    def _get_category(self, model):
-        if model not in self._categories:
-            category, is_new = Category.objects.get_or_create(name=model._meta.verbose_name)
-            self._categories[model] = category
-        return self._categories[model]
-
     def handle_label(self, label, **options):
         "Handles app_label or app_label.model_label formats."
         labels = label.split('.')
         models = None
-        create_categories = options.get('create_categories')
         include_non_editable = options.get('include_non_editable')
         include_keys = options.get('include_keys')
         update_existing = options.get('update_existing')
 
         if update_existing:
-            resp = raw_input('Are you sure you want to update existing metadata? '
-                'This will overwrite any previous changes made. Type "yes" to continue.')
+            resp = raw_input('Are you sure you want to update existing metadata?\n'
+                'This will overwrite any previous changes made. Type "yes" to continue: ')
             if resp.lower() != 'yes':
                 print 'Sync operation cancelled'
                 return
@@ -121,7 +102,6 @@ class Command(LabelCommand):
             new_count = 0
             update_count = 0
             model_name = model._meta.object_name.lower()
-            category = self._get_category(model) if create_categories else None
 
             for field in model._meta.fields:
                 if isinstance(field, ManyToManyField):
@@ -144,34 +124,29 @@ class Command(LabelCommand):
                     'field_name': field.name,
                 }
 
-                try:
-                    field = Field.objects.get(**lookup)
-                except Field.DoesNotExist:
-                    field = None
-
                 kwargs = {
                     'name': field.verbose_name.title(),
-                    'name_plural': field.verbose_name.title() + 's',
-                    'description': field.help_text,
+                    'description': field.help_text or None,
                     'app_name': app_name.lower(),
                     'model_name': model_name.lower(),
                     'field_name': field.name,
-                    'category': category,
                 }
 
-                if field:
+                try:
+                    field_obj = Field.objects.get(**lookup)
+                except Field.DoesNotExist:
+                    field_obj = Field(published=False, **kwargs)
+                    new_count += 1
+
+                if field_obj.pk:
                     if not update_existing:
                         print '(%s) %s.%s already exists. Skipping...' % (app_name, model_name, field.name)
                         continue
                     # Only overwrite if the source value is not falsy
-                    field.__dict__.update([(k, v) for k, v in kwargs.items() if v])
+                    field_obj.__dict__.update([(k, v) for k, v in kwargs.items()])
                     update_count += 1
-                else:
-                    field = Field(**kwargs)
-                    field.published = False
-                    new_count += 1
 
-                field.save()
+                field_obj.save()
 
 
             if new_count == 1:
