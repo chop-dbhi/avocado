b4e52ce2d5ce70a44bb427710a1b465437177114
Byron Ruth <b@devel.io>
2012-06-04 16:01:07 -0400

    Update test to conform to new translator output

--- avocado/tests/cases/query/translators.py
+++ avocado/tests/cases/query/translators.py
@@ -5,10 +5,10 @@ __all__ = ('TranslatorTestCase',)
 class TranslatorTestCase(BaseTestCase):
     def test(self):
         trans = self.is_manager.translate(value=False)
-        self.assertEqual(str(trans['condition']), "(AND: ('is_manager__exact', False))")
+        self.assertEqual(str(trans['query_modifiers']['condition']), "(AND: ('is_manager__exact', False))")
 
         trans = self.salary.translate(value=50000)
-        self.assertEqual(str(trans['condition']), "(AND: ('title__salary__exact', 50000.0), ('title__id__isnull', False))")
+        self.assertEqual(str(trans['query_modifiers']['condition']), "(AND: ('title__salary__exact', 50000.0), ('title__id__isnull', False))")
 
         trans = self.first_name.translate(value='Robert')
-        self.assertEqual(str(trans['condition']), "(AND: ('first_name__exact', u'Robert'))")
+        self.assertEqual(str(trans['query_modifiers']['condition']), "(AND: ('first_name__exact', u'Robert'))")
