efd1cd7e481b3f6f5c88b2d4e9f0adfcd3d8cf5b
Byron Ruth <b@devel.io>
2012-06-24 19:29:45 -0400

    Update and simplify setup.py and MANIFEST.in file Distribute is now used

--- setup.py
+++ setup.py
@@ -1,111 +1,66 @@
-import os
 import sys
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup, find_packages
 
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-
-BASE_PACKAGE = 'avocado'
-
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
-    # for this in distutils.command.install_data#306. It fixes install_lib but not
-    # install_data, which is why we roll our own install_data class.
-
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to the
-        # fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
-
-if sys.platform == "darwin":
-    cmdclasses = {'install_data': osx_install_data}
-else:
-    cmdclasses = {'install_data': install_data}
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk(BASE_PACKAGE):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-# Small hack for working with bdist_wininst.
-# See http://mail.python.org/pipermail/distutils-sig/2004-August/004134.html
-if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
-    for file_info in data_files:
-        file_info[0] = '\\PURELIB\\%s' % file_info[0]
-
-version = __import__(BASE_PACKAGE).get_version()
-
-install_requires = [
-    'distribute',
-    'django',
-    'modeltree',
-    'django-jsonfield',
-]
-
-# Ordereddict implementation must be installed for older versions
 if sys.version_info < (2, 7):
-    install_requires.append('ordereddict')
+    sys.stdout.write('Python versions < 2.7 are not supported\n')
+    sys.exit(1)
 
-setup(
-    version = version,
-    name = 'avocado',
-    author = 'Byron Ruth',
-    author_email = 'b@devel.io',
-    description = 'A metadata and data-driven query engine',
-    license = 'BSD',
-    keywords = 'query metadata',
-    url = 'https://github.com/cbmi/avocado/',
+kwargs = {
+    # Packages
+    'packages': find_packages(),
+    'include_package_data': True,
 
-    packages = packages,
-    cmdclass = cmdclasses,
-
-    install_requires = install_requires,
-    tests_require = ['coverage', 'django-haystack', 'whoosh', 'openpyxl'],
-
-    data_files = data_files,
-
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
+    # Dependencies
+    'install_requires': [
+        'django>=1.4',
+        'modeltree',
+        'django-jsonfield>=0.9',
     ],
-)
+
+    # Test dependencies
+    'tests_require': [
+        'coverage',
+        'django-haystack>=2.0',
+        'whoosh',
+        'openpyxl'
+    ],
+
+    # Optional dependencies
+    'extras_require': {
+        'django-guardian': [],
+        'django-haystack': [],
+        'openpyxl': [],
+        'scipy': ['numpy'],
+    },
+
+    # Resources unavailable on PyPi
+    'dependency_links': [
+        'https://github.com/bradjasper/django-jsonfield/tarball/master#egg=django-jsonfield-0.9',
+        'https://github.com/toastdriven/django-haystack/tarball/master#egg=django-haystack-2.0',
+    ],
+
+    # Metadata
+    'name': 'avocado',
+    'version': __import__('avocado').get_version(),
+    'author': 'Byron Ruth',
+    'author_email': 'b@devel.io',
+    'description': 'Metadata APIs for Django',
+    'license': 'BSD',
+    'keywords': 'query metadata',
+    'url': 'https://github.com/cbmi/avocado/',
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7'
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+    ],
+}
+
+setup(**kwargs)
