e0366d834f4313cb6367f917564f42c35fe6a5dc
Byron Ruth <b@devel.io>
2012-06-26 08:59:29 -0400

    SciPy must be installed after NumPy

--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@ import sys
 import distribute_setup
 distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
+from setuptools.command import install as _install
 
 if sys.version_info < (2, 7):
     sys.stdout.write('Python versions < 2.7 are not supported\n')
     sys.exit(1)
 
+
+class install(_install.install):
+    def run(self):
+        _install.install.run(self)
+        sys.stdout.write('NOTE: For `clustering` support, NumPy must '\
+            'installed first, followed by SciPy.\n')
+
 kwargs = {
+    'cmdclass': {'install': install},
+
     # Packages
     'packages': find_packages(),
     'include_package_data': True,
@@ -36,8 +46,9 @@ kwargs = {
         'permissions': ['django-guardian'],
         # Search
         'search': ['django-haystack>=2.0'],
-        # Clustering components
-        'clustering': ['numpy', 'scipy'],
+        # Clustering components.. unforunately SciPy must be installed separately
+        # since NumPy is a dependency
+        'clustering': ['numpy'],
         # Includes extra exporter dependencies
         'extras': ['openpyxl'],
     },
