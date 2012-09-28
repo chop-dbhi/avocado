40a53b2238b1b5b350ce3e87daa0c3cb2c0abed2
Byron Ruth <b@devel.io>
2012-09-28 09:43:20 -0400

    Prevent any test packages from being installed

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ kwargs = {
     'cmdclass': {'install': install},
 
     # Packages
-    'packages': find_packages(),
+    'packages': find_packages(exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
     'include_package_data': True,
 
     # Dependencies
