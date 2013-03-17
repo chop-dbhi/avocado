a0d00e818110823ab5337fbbe1b7cdcff8b923c9
Byron Ruth <b@devel.io>
2013-03-17 14:46:53 -0700

    Move interfaces under data package

--- tests/cases/interfaces/tests.py
+++ tests/cases/interfaces/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
-from avocado import interfaces
+from avocado.data import interfaces
 
 
 class InterfaceLoaderTestCase(TestCase):
     def test_get_interfaces(self):
         self.assertEqual(len(interfaces.get_interfaces()), 1)
-
-    
