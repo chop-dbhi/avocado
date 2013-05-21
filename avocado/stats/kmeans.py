8a0863fb20c33dd61a841b4fedda71ce2c704484
Don Naegely <naegelyd@gmail.com>
2013-05-21 16:19:17 -0400

    Add vector quantization method and tests.
    This commit also removes the previous kmeans method. I'll be adding a
    new one soon that works based on code books(centroids) and leverages the
    newly added vector quantization algorithm.

--- avocado/stats/kmeans.py
+++ avocado/stats/kmeans.py
@@ -1,9 +1,5 @@
 import math
 from random import random
-from collections import namedtuple
-
-Point = namedtuple('Point', ('coords', 'n', 'ct'))
-Cluster = namedtuple('Cluster', ('points', 'center', 'n'))
 
 def std_dev(values):
     """
@@ -76,45 +72,145 @@ def whiten(points):
     # deviation.
     return [[p[i] / std_devs[i] for i in dimension_indeces] for p in points]
 
-def sqr_euclidean(p1, p2):
+def get_dimension(observations):
     """
-    Computes the squared Euclidean distance between 'p1' and 'p2'.
+    Returns the dimension of the observations list.
+
+    The dimension is considered the number of columns or features that make
+    up the observations. For example, the following observation list would
+    have dimension 3.
+
+        obs = [[1., 2., 3.],
+               [4., 5., 6.]]
+
+    This method checks that all observations have the same dimension and will
+    raise a ValueError if all the observations do not share the same dimension.
+
+    Arguments:
+        observations: list of observations of any dimension
+
+    Returns:
+        The dimension of the observations list(aka the number of columns) or
+        raises a ValueError if the number of dimensions is not consistent
+        over all observations.
     """
-    return sum([(p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)])
 
-def calculate_center(points, n):
-    vals = [0.0 for i in range(n)]
-    plen = 0
-    for p in points:
-        plen += p.ct
-        for i in range(n):
-            vals[i] += (p.coords[i] * p.ct)
-    return Point([(v / plen) for v in vals], n, 1)
+    if isinstance(observations[0], list):
+        dimension = len(observations[0])
+    else:
+        dimension = 1
 
-def kmeans(points, k, min_diff):
-    clusters = [Cluster([p], p, p.n) for p in random.sample(points, k)]
+    i_dimension = -1
+    for i in range(len(observations)):
+        if isinstance(observations[i], list):
+            i_dimension = len(observations[i])
+        else:
+            i_dimension = 1
+        
+        if i_dimension != dimension:
+            raise ValueError("Observations do not have a consistent dimension.")
 
-    while True:
-        plists = [[] for i in range(k)]
+    return dimension
+    
+def observation_sqr_euclidean(obs1, obs2):
+    """
+    Calculates the square Euclidean distance for each feature.
 
-        for p in points:
-            smallest_distance = float('Inf')
-            for i in range(k):
-                distance = sqr_euclidean(p, clusters[i].center)
-                if distance < smallest_distance:
-                    smallest_distance = distance
-                    idx = i
-            plists[idx].append(p)
+    The returned value is a list of distances representing the squared
+    Euclidean distance along each feature of the observations. This is shown
+    in the example below:
 
-        diff = 0
-        for i in range(k):
-            old = clusters[i]
-            center = calculate_center(plists[i], old.n)
-            new = Cluster(plists[i], center, old.n)
-            clusters[i] = new
-            diff = max(diff, sqr_euclidean(old.center, new.center))
+        o1 = [1, 2, 3]
+        o2 = [3, 6, 9]
+        observation_sqr_euclidean(o1, o2)
 
-        if diff < min_diff:
-            break
+        #   [4, 14, 36]
+    
+    Returns:
+        The square Euclidean distance along each feature of supplied 
+        observations.
+    """
+    return [(o1 - o2) ** 2 for o1, o2 in zip(obs1, obs2)]
 
-    return clusters
+def index_of_min(values):
+    """
+    Finds and returns the index of the smallest item in the 'values' list.
+
+    If multiple values are all equal to the minimum value then the one with
+    the smallest index is returned.
+    """
+    min_value = float('Inf')
+    min_index = 0
+    
+    for i in range(len(values)):
+        if values[i] < min_value:
+            min_value = values[i]
+            min_index = i
+    
+    return min_index
+
+def vq(observations, codebook):
+    """
+    Vector quantization algorithm.
+
+    Computes the Euclidean distance between each observation and each entry in 
+    the code book. See below links for general VQ description:
+
+    http://www.oocities.org/stefangachter/VectorQuantization/chapter_1.htm
+    http://www.data-compression.com/vq.html
+
+    Returns:
+        encodings: A list of encodings such that encodings[i] is the encoding
+                   of the ith observation.
+        distances: A list of distances such that distances[i] represents the 
+                   minimum distance between the ith observation and its
+                   encoding.
+    """
+    
+    if len(observations) < 1:
+        raise ValueError("vq requires at least one observation. 0 observations found.")
+    if len(codebook) < 1:
+        raise ValueError("vq requires at least one codebook vector. 0 codebook entries found.")
+    # Create copies of the observations and codebook just in case we need to 
+    # transform to multi-dimensional.
+    obs = list(observations)
+    cb = list(codebook)
+
+    # Since this method is meant to remove the dependency on scipy and scipy 
+    # has no support for 1d arrays, we modify 1d lists to multi-dimensional 
+    # lists in order to reuse the same multi-dimensional VQ algorithm and then 
+    # simply flatten the result later in the case of 1d lists.
+    # TODO: Write a Scalar Quantization method or make this 1d compatible
+    #       without this hack.
+    d = get_dimension(obs)
+    if d == 1:
+        obs = [[o] for o in observations]
+        cb = [[c] for c in codebook]
+
+    n = len(obs)
+
+    if d != get_dimension(cb):
+        raise ValueError("observations and codebook must have the same number of features(columns) per observation")
+
+    encodings = [0] * n
+    min_distances = [0] * n
+    for i in range(n):
+        # Compute the squared Euclidean distance between this observation and
+        # every entry in the code book.
+        distances = [observation_sqr_euclidean(obs[i], cb[j]) for j in range(len(cb))]
+        
+        # Sum across all dimensions of each distance measure.
+        distance_totals = [sum(distances[j]) for j in range(len(distances))]
+
+        # Find the index containing the minimum distance. This provides a 
+        # decoding from the codevector to the output vector.
+        encodings[i] = index_of_min(distance_totals)
+
+        # Save the minimum distance to the output vector for the given 
+        # codevector from the codebook and the encoding index.
+        min_distances[i] = distance_totals[encodings[i]]
+        
+    # Do the square root now to get the Euclidean distance. We don't do this
+    # in the loop so that we can save time by not taking the square root of 
+    # non-minimal distances.
+    return encodings, [math.sqrt(d) for d in min_distances]
--- tests/cases/stats/tests/kmeans.py
+++ tests/cases/stats/tests/kmeans.py
@@ -1,8 +1,9 @@
 import os
+import random
 from django.test import TestCase
 from avocado.stats import cluster, kmeans
 from scipy.cluster import vq
-import numpy
+import numpy as np
 from itertools import chain
 
 __all__ = ('KmeansTestCase',)
@@ -14,13 +15,13 @@ random_points_3d = [[float(x) for x in l.strip().split(",")] for l in random_poi
 
 class KmeansTestCase(TestCase):
     def test_std_dev(self):
-        numpy_std_dev = numpy.std(numpy.array(random_points))
+        numpy_std_dev = np.std(np.array(random_points))
         our_std_dev = kmeans.std_dev(random_points)
 
         self.assertEqual(numpy_std_dev, our_std_dev)
 
     def test_whiten(self):
-        scipy_whiten = vq.whiten(numpy.array(random_points))
+        scipy_whiten = vq.whiten(np.array(random_points))
         our_whiten = kmeans.whiten(random_points)
         
         self.assertEqual(len(scipy_whiten), len(our_whiten))
@@ -29,7 +30,7 @@ class KmeansTestCase(TestCase):
 
         [self.assertEqual(*comp) for comp in comp_whiten]
         
-        scipy_whiten = vq.whiten(numpy.array(random_points_3d))
+        scipy_whiten = vq.whiten(np.array(random_points_3d))
         our_whiten = kmeans.whiten(random_points_3d)
         
         self.assertEqual(len(scipy_whiten), len(our_whiten))
@@ -37,3 +38,49 @@ class KmeansTestCase(TestCase):
         comp_whiten = zip(scipy_whiten, our_whiten)
 
         [self.assertSequenceEqual(scipy_list.tolist(), our_list) for scipy_list, our_list in comp_whiten]
+
+    def test_scipy_vq_versions(self):
+        vq_points = np.array([[ 1.9,2.3],
+                              [ 1.5,2.5],
+                              [ 0.8,0.6],
+                              [ 0.4,1.8],
+                              [ 1.0,1.0]])
+        book = np.array((vq_points[0], vq_points[2]))
+
+        c_code, c_dist = vq.vq(vq_points, book)
+        p_code, p_dist = vq.py_vq(vq_points, book)
+
+        self.assertSequenceEqual(c_code.tolist(), p_code.tolist())
+        self.assertSequenceEqual(c_dist.tolist(), p_dist.tolist())
+
+        one_dim_points = [1.9, 2.3, 1.5, 2.5, 0.8, 0.6, 0.4, 1.8, 1.0, 1.0]
+        vq_points = np.array([[p] for p in one_dim_points])
+        book = np.array([vq_points[3], vq_points[7]])
+
+        c_code, c_dist = vq.vq(vq_points, book)
+        p_code, p_dist = vq.py_vq(vq_points, book)
+
+        self.assertSequenceEqual(c_code.tolist(), p_code.tolist())
+        self.assertSequenceEqual(c_dist.tolist(), p_dist.tolist())
+
+    def test_vq(self):
+        book = [p for p in random.sample(random_points, 8)]
+
+        # SciPy doesn't work with 1d arrays yet so the 1d test data needs to
+        # be transformed to a multidimensional representation
+        one_d_array = np.array([[p] for p in random_points])
+        one_d_book_array = np.array([[b] for b in book])
+
+        s_code, s_dist = vq.vq(one_d_array, one_d_book_array)
+        m_code, m_dist = kmeans.vq(random_points, book)
+
+        self.assertSequenceEqual(s_code.tolist(), m_code)
+        self.assertSequenceEqual(s_dist.tolist(), m_dist)
+
+        book = [p for p in random.sample(random_points_3d, 8)]
+
+        s_code, s_dist = vq.vq(np.array(random_points_3d), np.array(book))
+        m_code, m_dist = kmeans.vq(random_points_3d, book)
+
+        self.assertSequenceEqual(s_code.tolist(), m_code)
+        self.assertSequenceEqual(s_dist.tolist(), m_dist)
