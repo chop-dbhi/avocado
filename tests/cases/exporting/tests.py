191d5ceb02b7c5075242dee18e642b34a36ed5b6
Don Naegely <naegelyd@gmail.com>
2013-06-03 16:21:27 -0400

    Increase upper bound on size in excel export tests
    Given this constraint in setup.py -- 'openpyxl>=1.6,<1.7' -- openpyxl 1.6.2 is getting installed during Travis CI builds. This version returns content slightly larger(4 bytes) than the previous upper bound. I increased the upper bound 10 bytes to account for variability in openpyxl versions.

--- tests/cases/exporting/tests.py
+++ tests/cases/exporting/tests.py
@@ -87,7 +87,7 @@ class FileExportTestCase(TestCase):
         self.assertTrue(os.path.exists(fname))
         # Observed slight size differences..
         l = len(open(fname).read())
-        self.assertTrue(6220 <= l <= 6240)
+        self.assertTrue(6220 <= l <= 6250)
         os.remove(fname)
 
     def test_sas(self):
@@ -141,7 +141,7 @@ class ResponseExportTestCase(FileExportTestCase):
         exporter.write(self.query, response)
         # Observed slight size differences..
         l = len(response.content)
-        self.assertTrue(6220 <= l <= 6240)
+        self.assertTrue(6220 <= l <= 6250)
 
     def test_sas(self):
         exporter = export.SASExporter(self.concepts)
