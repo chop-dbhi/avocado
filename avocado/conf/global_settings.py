e02b6920eb6e31ec607cebffa9c371ed1d501c28
Don Naegely <naegelyd@gmail.com>
2013-06-27 13:22:40 -0400

    Support exclusive range on date, time, datetime, and number fields

--- avocado/conf/global_settings.py
+++ avocado/conf/global_settings.py
@@ -35,11 +35,11 @@ SIMPLE_TYPE_MAP = {
 OPERATOR_MAP = {
     'key': ('exact', '-exact', 'in', '-in'),
     'boolean': ('exact', '-exact', 'in', '-in'),
-    'date': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range'),
-    'number': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range'),
+    'date': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range', '-range'),
+    'number': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range', '-range'),
     'string': ('exact', '-exact', 'iexact', '-iexact', 'in', '-in', 'icontains', '-icontains'),
-    'datetime': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range'),
-    'time': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range'),
+    'datetime': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range', '-range'),
+    'time': ('exact', '-exact', 'in', '-in', 'lt', 'lte', 'gt', 'gte', 'range', '-range'),
 }
 
 # A general mapping of formfield overrides for all subclasses. the mapping is
