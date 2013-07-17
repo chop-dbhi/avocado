b357a44decf90df484dac2016349ed4bf76d12a1
Byron Ruth <b@devel.io>
2013-07-16 23:19:01 -0400

    Use newline delimited list of apps for readability

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,20 @@
 ARGS="$@"
 
 if [ ! $ARGS ]; then
-    ARGS="avocado core exporting formatters lexicon events models query sets stats search subcommands"
+read -d '' ARGS <<- EOF
+    avocado
+    core
+    exporting
+    formatters
+    lexicon
+    events
+    models
+    query
+    sets
+    stats
+    search
+    subcommands
+EOF
 fi
 
 DJANGO_SETTINGS_MODULE='tests.settings' PYTHONPATH=. coverage run `which django-admin.py` test $ARGS
--- test_suite.py
+++ test_suite.py
@@ -3,6 +3,20 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 from django.core import management
-management.call_command('test', 'core', 'exporting', 'formatters',
-    'lexicon', 'events', 'models', 'query', 'sets', 'stats', 'search',
-    'subcommands')
+
+apps = [
+    'test',
+    'core',
+    'exporting',
+    'formatters',
+    'lexicon',
+    'events',
+    'models',
+    'query',
+    'sets',
+    'stats',
+    'search',
+    'subcommands',
+]
+
+management.call_command(*apps)
