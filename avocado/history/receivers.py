055212fd2f2e069853479d983e09aee7ca0f44f4
Byron Ruth <b@devel.io>
2013-08-26 11:15:25 -0400

    Extract user and session_key from instance on history receivers
    This ensures the user and/or session_key is propagated across revisions.

--- avocado/history/receivers.py
+++ avocado/history/receivers.py
@@ -4,10 +4,16 @@ from .models import Revision
 def post_save_revision(instance, created, **kwargs):
     from . import registry
     fields = registry[instance.__class__]['fields']
-    Revision.objects.create_revision(instance, fields=fields)
+    user = getattr(instance, 'user')
+    session_key = getattr(instance, 'session_key')
+    Revision.objects.create_revision(instance, fields=fields, user=user,
+        session_key=session_key)
 
 
 def pre_delete_revision(instance, **kwargs):
     from . import registry
     fields = registry[instance.__class__]['fields']
-    Revision.objects.create_revision(instance, fields=fields, deleted=True)
+    user = getattr(instance, 'user')
+    session_key = getattr(instance, 'session_key')
+    Revision.objects.create_revision(instance, fields=fields, deleted=True,
+        user=user, session_key=session_key)
