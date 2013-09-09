cb35c28ac04c4cfb6170d44b602403dc3cbf7205
Byron Ruth <b@devel.io>
2013-09-09 08:20:20 -0400

    Remove requirement in DataFieldCommand to pass labels
    The behavior is now (naturally) defaults to all datafields

--- avocado/management/base.py
+++ avocado/management/base.py
@@ -3,15 +3,12 @@ from .utils import get_fields_by_label
 
 
 class DataFieldCommand(BaseCommand):
-    args = '(app | app.model | app.model.field)*'
+    args = '[app | app.model | app.model.field]*'
 
     def handle_fields(self, *args, **kwargs):
         raise NotImplemented('Subclasses must define this method.')
 
     def handle(self, *labels, **kwargs):
-        if not labels:
-            raise CommandError('At least one label for looking up DataFields must be specified.')
-
         try:
             fields = get_fields_by_label(labels)
         except Exception, e:
