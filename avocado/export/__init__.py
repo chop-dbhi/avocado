0e68fd50428ceaf53e00e22c11a45ec98185e738
Byron Ruth <b@devel.io>
2014-03-28 06:47:15 -0400

    Replace exporter registry keys with short_name derivative
    Fix #203
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- avocado/export/__init__.py
+++ avocado/export/__init__.py
@@ -5,18 +5,18 @@ from _csv import CSVExporter
 from _sas import SASExporter
 from _r import RExporter
 from _json import JSONExporter
-from _html import HTMLExporter      # noqa
+from _html import HTMLExporter  # noqa
 
 registry = loader.Registry(register_instance=False)
 
-registry.register(CSVExporter, 'csv')
-registry.register(SASExporter, 'sas')
-registry.register(RExporter, 'r')
-registry.register(JSONExporter, 'json')
-# registry.register(HTMLExporter, 'html')
+registry.register(CSVExporter, CSVExporter.short_name.lower())
+registry.register(SASExporter, SASExporter.short_name.lower())
+registry.register(RExporter, RExporter.short_name.lower())
+registry.register(JSONExporter, JSONExporter.short_name.lower())
+# registry.register(HTMLExporter, HTMLExporter.short_name.lower())
 
 if OPTIONAL_DEPS['openpyxl']:
     from _excel import ExcelExporter
-    registry.register(ExcelExporter, 'excel')
+    registry.register(ExcelExporter, ExcelExporter.short_name.lower())
 
 loader.autodiscover('exporters')
