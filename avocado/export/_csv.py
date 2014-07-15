2fc9b7e534bfc910fc145754e450cbed0757b1c1
Don Naegely <naegelyd@gmail.com>
2014-07-15 11:40:53 -0400

    Support Unicode input in the CSV exporter
    Signed-off-by: Don Naegely <naegelyd@gmail.com>

--- avocado/export/_csv.py
+++ avocado/export/_csv.py
@@ -2,6 +2,27 @@ import csv
 from _base import BaseExporter
 
 
+class UnicodeWriter(object):
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+
+    Adapted from https://github.com/jdunck/python-unicodecsv/blob/master/unicodecsv/__init__.py   # noqa
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding='utf-8', *args, **kwds):
+        self.encoding = encoding
+        self.writer = csv.writer(f, dialect, *args, **kwds)
+
+    def writerow(self, row):
+        self.writer.writerow(
+            [s.encode("utf-8") if 'encode' in dir(s) else s for s in row])
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+
 class CSVExporter(BaseExporter):
     short_name = 'CSV'
     long_name = 'Comma-Separated Values (CSV)'
@@ -14,7 +35,7 @@ class CSVExporter(BaseExporter):
     def write(self, iterable, buff=None, *args, **kwargs):
         header = []
         buff = self.get_file_obj(buff)
-        writer = csv.writer(buff, quoting=csv.QUOTE_MINIMAL)
+        writer = UnicodeWriter(buff, quoting=csv.QUOTE_MINIMAL)
 
         for i, row_gen in enumerate(self.read(iterable, *args, **kwargs)):
             row = []
