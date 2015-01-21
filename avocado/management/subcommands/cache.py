22f87da589fd8009355b701872461b77f9c3ca08
Byron Ruth <b@devel.io>
2015-01-20 21:10:28 -0500

    Clean up cache subcommand to continue after failure
    Signed-off-by: Byron Ruth <b@devel.io>

--- avocado/management/subcommands/cache.py
+++ avocado/management/subcommands/cache.py
@@ -2,7 +2,7 @@ import sys
 import time
 import logging
 from optparse import make_option
-from django.db import DatabaseError
+from django.db import transaction
 from django.core.management.base import BaseCommand, CommandError
 from avocado.models import DataField
 from avocado.management.base import DataFieldCommand
@@ -22,6 +22,8 @@ for attr_name in dir(DataField):
 
 CACHED_METHODS = tuple(CACHED_METHODS)
 
+METHOD_CHOICES = ', '.join(CACHED_METHODS)
+
 
 __doc__ = """\
 Pre-caches data produced by various DataField methods that are data dependent.
@@ -33,18 +35,33 @@ class Command(DataFieldCommand):
     help = __doc__
 
     option_list = BaseCommand.option_list + (
-        make_option('--flush', action='store_true', help='Flushes existing '
-                    'cache for each cached property.'),
+        make_option('--flush',
+                    action='store_true',
+                    help='Flushes existing cache for each cached property.',
+                    default=False),
 
-        make_option('--methods', action='append', dest='methods',
-                    default=CACHED_METHODS, help='Select which methods to '
-                    'pre-cache. Choices: {0}'.format(
-                        ', '.join(CACHED_METHODS))),
+        make_option('--methods',
+                    action='append',
+                    dest='methods',
+                    default=CACHED_METHODS,
+                    help='Select which methods to pre-cache. Choices: {0}'
+                         .format(METHOD_CHOICES)),
     )
 
-    def _progress(self):
-        sys.stdout.write('.')
-        sys.stdout.flush()
+    @transaction.commit_on_success
+    def _handle_field_method(self, f, method, flush):
+        func = getattr(f, method)
+
+        if flush:
+            func.flush(f)
+
+        if func.cached(f):
+            self.skipped += 1
+        else:
+            data = func()
+
+            if data is not None:
+                self.cached += 1
 
     def handle_fields(self, fields, **options):
         flush = options.get('flush')
@@ -54,32 +71,34 @@ class Command(DataFieldCommand):
         for method in methods:
             if method not in CACHED_METHODS:
                 raise CommandError('Invalid method {0}. Choices are {1}'
-                                   .format(method, ', '.join(CACHED_METHODS)))
+                                   .format(method, METHOD_CHOICES))
 
-        count = 0
+        self.total = 0
+        self.skipped = 0
+        self.cached = 0
+        self.errors = 0
+
         t0 = time.time()
 
         for f in fields:
             for method in methods:
-                func = getattr(f, method)
-
-                if flush:
-                    func.flush(f)
+                self.total += 1
 
                 # By default, the settings run on sqlite3 DB so a
                 # DatabaseError will be triggered when the standard deviation
                 # or variance functions are used.
                 try:
-                    func()
-                except DatabaseError:
-                    pass
+                    self._handle_field_method(f, method, flush)
+                except Exception:
+                    self.errors += 1
+                    log.exception('error populating cache for "{0}" {1}'
+                                  .format(f, method))
 
-            count += 1
+            if self.total % 10 == 0:
+                sys.stdout.write('\r{0}/{1}/{2}/{3} '
+                                 'cached/skipped/errors/total'
+                                 .format(self.cached, self.skipped,
+                                         self.errors, self.total))
+                sys.stdout.flush()
 
-            self._progress()
-
-            log.debug('{0} cache set took {1} seconds'.format(
-                f, time.time() - t0))
-
-        print(u'\n{0} fields have been updated ({1} s)'.format(
-            count, round(time.time() - t0, 2)))
+        print('\nTook {0} s'.format(round(time.time() - t0, 2)))
